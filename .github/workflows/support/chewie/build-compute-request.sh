44ad3d6cb7a94410e69a0489ab7ae73f9e6c42a6
Roger Barker <roger.barker@swirldslabs.com>
2026-07-22 13:19:32 -0500

    chore: Add duration seconds in 859
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- .github/workflows/support/chewie/build-compute-request.sh
+++ .github/workflows/support/chewie/build-compute-request.sh
@@ -2,9 +2,14 @@
  set -euo pipefail
 
 build_compute_request() {
-  local QUANTITY=0
-  local CPU=0
-  local MEMORY=0
+  local CONSENSUS_GROUP_NAME=""
+  local AUXILIARY_GROUP_NAME=""
+  local CN_QUANTITY=0
+  local AUX_QUANTITY=0
+  local CN_CPU=0
+  local AUX_CPU=0
+  local CN_MEMORY=0
+  local AUX_MEMORY=0
   local DURATION=0
   local RUN_ID=0
   local RUN_NUMBER=0
@@ -12,20 +17,25 @@ build_compute_request() {
   local OWNER=""
   local REPOSITORY=""
   local JOB=""
-  while getopts "q:c:m:d:i:n:a:o:r:j:h" arg; do
+  while getopts "d:q:c:m:g:a:p:w:x:i:n:t:o:r:j:h" arg; do
     case $arg in
-      q) QUANTITY=${OPTARG} ;;
-      c) CPU=${OPTARG} ;;
-      m) MEMORY=${OPTARG} ;;
       d) DURATION=${OPTARG} ;;
+      q) CN_QUANTITY=${OPTARG} ;;
+      c) CN_CPU=${OPTARG} ;;
+      m) CN_MEMORY=${OPTARG} ;;
+      g) CONSENSUS_GROUP_NAME="${OPTARG}" ;;
+      a) AUX_QUANTITY=${OPTARG} ;;
+      p) AUX_CPU=${OPTARG} ;;
+      w) AUX_MEMORY=${OPTARG} ;;
+      x) AUXILIARY_GROUP_NAME="${OPTARG}" ;;
       i) RUN_ID=${OPTARG} ;;
       n) RUN_NUMBER=${OPTARG} ;;
-      a) RUN_ATTEMPT=${OPTARG} ;;
+      t) RUN_ATTEMPT=${OPTARG} ;;
       o) OWNER="${OPTARG}" ;;
       r) REPOSITORY="${OPTARG}" ;;
       j) JOB="${OPTARG}" ;;
       h)
-        echo "Usage: ${0} -q <quantity> -c <cpu> -m <memory> -d <duration> -i <run_id> -n <run_number> -a <run_attempt> -o <owner> -r <repository> -j <job>"
+        echo "Usage: ${0} -q <cn_quantity> -c <cn_cpu> -m <cn_memory> -d <duration> -g <consensus-group-name> -a <aux_quantity> -p <aux_cpu> -w <aux_memory> -x <auxiliary-group-name> -i <run_id> -n <run_number> -t <run_attempt> -o <owner> -r <repository> -j <job>"
         exit 0
         ;;
       *)
@@ -36,23 +46,39 @@ build_compute_request() {
   done
 
   # validate that all required options are provided
-  if [[ "$QUANTITY" == "0" ]] || [[ "$CPU" == "0" ]] || \
-     [[ "$MEMORY" == "0" ]] || [[ "$DURATION" == "0" ]] || \
-     [[ "$RUN_ID" == "0" ]] || [[ "$RUN_NUMBER" == "0" ]] || \
+  if [[ "$CN_QUANTITY" == "0" ]] || [[ "$CN_CPU" == "0" ]] || [[ "$CN_MEMORY" == "0" ]] || \
+     [[ "$AUX_QUANTITY" == "0" ]] || [[ "$AUX_CPU" == "0" ]] || [[ "$AUX_MEMORY" == "0" ]] || \
+     [[ "$DURATION" == "0" ]] || [[ "$RUN_ID" == "0" ]] || [[ "$RUN_NUMBER" == "0" ]] || \
      [[ "$RUN_ATTEMPT" == "0" ]] || [[ -z "$OWNER" ]] || \
      [[ -z "$REPOSITORY" ]] || [[ -z "$JOB" ]]; then
     echo "Error: Missing required options"
     exit 1
   fi
 
+  if [[ "${CONSENSUS_GROUP_NAME:-}" == "" ]]; then
+    CONSENSUS_GROUP_NAME="cn-nodes"
+  fi
+
+  if [[ "${AUXILIARY_GROUP_NAME:-}" == "" ]]; then
+    AUXILIARY_GROUP_NAME="aux-nodes"
+  fi
+
   # build curl body into a variable
   local REQUEST_BODY
   REQUEST_BODY=$(cat <<EOF
 {
-  "instances":[{
-    "quantity":${QUANTITY},
-    "resources":{"cpu":${CPU},"memory":${MEMORY}}
-  }],
+  "instances":[
+    {
+      "group": "${CONSENSUS_GROUP_NAME}",
+      "quantity":${CN_QUANTITY},
+      "resources":{"cpu":${CN_CPU},"memory":${CN_MEMORY}}
+    },
+    {
+      "group": "${AUXILIARY_GROUP_NAME}",
+      "quantity":${AUX_QUANTITY},
+      "resources":{"cpu":${AUX_CPU},"memory":${AUX_MEMORY}}
+    }
+  ],
   "duration":${DURATION},
   "workflow":{
     "run":{
