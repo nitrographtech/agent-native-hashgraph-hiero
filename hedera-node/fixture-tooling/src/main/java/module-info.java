091e461225ce9329c97bfa41f63913224e724a84
Eric Faust <195801021+nitro-corporation@users.noreply.github.com>
2026-07-24 13:17:19 -0700

    Fix fixture tooling JPMS scope

--- hedera-node/fixture-tooling/src/main/java/module-info.java
+++ hedera-node/fixture-tooling/src/main/java/module-info.java
@@ -3,14 +3,20 @@ module com.hedera.node.fixture.tooling {
     uses com.hedera.node.app.service.contract.impl.exec.ActionSidecarContentTracerFactory;
 
     requires com.hedera.node.app.service.contract.impl;
+    requires com.hedera.node.app.service.entityid;
+    requires com.hedera.node.hapi;
     requires com.hedera.node.test.clients;
     requires com.hedera.pbj.runtime;
     requires com.swirlds.platform.core;
+    requires com.github.spotbugs.annotations;
     requires org.hiero.consensus.model;
+    requires org.hyperledger.besu.datatypes;
+    requires org.hyperledger.besu.evm;
     requires java.base;
     requires org.apache.logging.log4j;
     requires org.bouncycastle.provider;
     requires org.junit.jupiter.api;
+    requires tuweni.bytes;
 
     exports com.hedera.services.bdd.fixturetooling.tracing to
             com.hedera.node.app.service.contract.impl;
